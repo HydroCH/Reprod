8ec9ebbc93b9169f7abe597351983096ac3c91a0
GlazioCH <71430189+HydroCH@users.noreply.github.com>
2023-05-25 10:30:12 +0200

    Update 10:30

--- glacierAccumulationRate.m
+++ glacierAccumulationRate.m
@@ -1,13 +1,13 @@
-function accumulationRate = glacierAccumulationRate(precipitation, snowfallTemperature)
+function accumulationRate = glacierAccumulationRate(precipitation, Temperature)
     % Constants
     densityWater = 1000;  % kg/m^3
     densitySnow = 300;  % kg/m^3
 
     % Calculate accumulation rate
-    temperatureDifference = snowfallTemperature - 0;  % Difference from melting point of ice (0 degrees Celsius)
+    X = Temperature;  
     snowfallVolume = precipitation / densityWater;  % Assume constant density of water
     snowfallMass = snowfallVolume * densitySnow;
-    accumulationRate = snowfallMass * (1 - 0.01 * temperatureDifference);
+    accumulationRate = snowfallMass * (1 - 0.01 * X);
 end
 
 % This function takes two input parameters: precipitation (in millimeters) and snowfallTemperature (in degrees Celsius).
